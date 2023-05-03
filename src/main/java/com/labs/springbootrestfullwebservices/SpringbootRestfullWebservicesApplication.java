50a1928ac61cc5f52d8066f5311b9ae19aeb0774
hadipsy27 <hadipsy27@gmail.com>
2023-05-03 13:29:28 +0700

    First Commit

---
+++ src/main/java/com/labs/springbootrestfullwebservices/SpringbootRestfullWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.labs.springbootrestfullwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfullWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfullWebservicesApplication.class, args);
+	}
+
+}
