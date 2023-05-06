c165f78b3aff8fcbb14cb7b668a2054cbecf1caa
hadipsy27 <hadipsy27@gmail.com>
2023-05-06 17:44:33 +0700

    Implement Customizing validation Error Response with GlobalExceptionHandler

--- src/main/java/com/labs/springbootrestfullwebservices/dto/UserDto.java
+++ src/main/java/com/labs/springbootrestfullwebservices/dto/UserDto.java
@@ -12,16 +12,16 @@ import lombok.NoArgsConstructor;
 public class UserDto {
 
     private Long id;
-    // first name should not be null or empty
-    @NotEmpty
+    // User first name should not be null or empty
+    @NotEmpty(message = "First name should not be null or empty")
     private String firstName;
     // last name should not be null or empty
-    @NotEmpty
+    @NotEmpty(message = "Last name should not be null or empty")
     private String lastName;
 
     // email should not be null or empty
     // email address shoud be valid
-    @NotEmpty
-    @Email
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email(message = "Email should be valid")
     private String email;
 }
